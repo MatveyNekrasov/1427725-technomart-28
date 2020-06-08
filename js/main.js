10e3f723dd44094aa7d44dea5cc74469fde5ae3c
Matvey Nekrasov Home <metal59@gmail.com>
2020-06-08 19:31:34 +0700

    feat: добавлена смена слайдов в промоблоке

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // ---------------------- Modals
 
 function modalHandler(modalName, openBtnsName, closeBtnsName) {
@@ -48,19 +50,79 @@ modalHandler(
   ".contacts__btn-writeus",
   ".modal-writeus__close-js");
 
-// ---------------------- Services
+// ---------------------- Promo slider
+
+let promoSliders = document.querySelectorAll(".promo-slider");
+let promoKnobs = document.querySelectorAll(".promo-sliders__input");
+
+// change slider by changing the radio item
+
+for (let i = 0; i < promoKnobs.length; i++) {
+  promoKnobs[i].addEventListener("change", function (e) {
+    e.preventDefault();
+    // show corresponding list item
+    for (let j = 0; j < promoSliders.length; j++) {
+      promoSliders[j].classList.remove("promo-slider--visible");
+      if (j === i) {
+        promoSliders[j].classList.add("promo-slider--visible");
+      }
+    }
+  })
+}
+
+// change slider by clicking next/prev button
+
+function slideButtonsHandler(buttonSelector, typePrevNext) {
+  if (typePrevNext !== "prev" && typePrevNext !== "next")
+    return;
+
+  let promoBtn = document.querySelector(buttonSelector);
+  promoBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let currentSlideNum = 0;
+
+    for (let i = 0; i < promoKnobs.length; i++) {
+      if (promoKnobs[i].checked) {
+        currentSlideNum = i;
+        break;
+      }
+    }
+
+    if (typePrevNext === "prev" && currentSlideNum > 0) {
+      currentSlideNum--;
+      promoKnobs[currentSlideNum].checked = true;
+    } else if (typePrevNext === "next" && currentSlideNum < promoKnobs.length - 1) {
+      currentSlideNum++;
+      promoKnobs[currentSlideNum].checked = true;
+    }
+
+    // show corresponding list item
+    for (let j = 0; j < promoSliders.length; j++) {
+      promoSliders[j].classList.remove("promo-slider--visible");
+      if (j === currentSlideNum) {
+        promoSliders[j].classList.add("promo-slider--visible");
+      }
+    }
+
+  });
+}
+
+slideButtonsHandler(".promo-sliders__btn-prev", "prev");
+slideButtonsHandler(".promo-sliders__btn-next", "next");
+
+// ---------------------- Services slider
 
 let servicesBtns = document.querySelectorAll(".btn-services");
 let servicesItems = document.querySelectorAll(".services__item");
 
-for (let i = 0; i < servicesBtns.length; i++) {  
+for (let i = 0; i < servicesBtns.length; i++) {
   servicesBtns[i].addEventListener("click", function (e) {
     e.preventDefault();
 
     // make corresponding button active
     for (let j = 0; j < servicesBtns.length; j++) {
       servicesBtns[j].classList.remove("btn-services--active");
-      if (i === j) {
+      if (j === i) {
         servicesBtns[j].classList.add("btn-services--active");
       }
     }
@@ -68,7 +130,7 @@ for (let i = 0; i < servicesBtns.length; i++) {
     // show corresponding list item
     for (let j = 0; j < servicesItems.length; j++) {
       servicesItems[j].classList.remove("services__item--visible");
-      if (i === j) {
+      if (j === i) {
         servicesItems[j].classList.add("services__item--visible");
       }
     }
